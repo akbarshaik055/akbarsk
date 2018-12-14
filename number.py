c2935cfac00e55c27921f09b9d07c58dbd0512da
akbarshaik055 <42571502+akbarshaik055@users.noreply.github.com>
2018-12-14 10:55:41 +0530

    Update number.py

--- number.py
+++ number.py
@@ -2,5 +2,5 @@ input_string = input("Enter a string : ")
 count = 0
 for c in input_string :
   if c.isspace() != True:
-    count = count + 1
+    count = count + 5
 print("Total number of characters : ",count)
