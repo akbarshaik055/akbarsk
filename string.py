11b7f09dbaa880b45566f8f67ad9b08a74a55642
akbarshaik055 <42571502+akbarshaik055@users.noreply.github.com>
2018-12-14 10:48:08 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,8 @@
+# Python code for implementation of isnumeric() 
+    
+# checking for numeric characters 
+string = '123ayu456'
+print(string.isnumeric()) 
+   
+string = '78604'
+print( string.isnumeric()) 
